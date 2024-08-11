611f9b7c3575fdb8ffdd84999aa5da86d6bc487b
Vuyelwa Mavuma <mavumavuyelwa@gmail.com>
2024-08-11 20:40:49 +0200

    Correct errors

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
+import React from "react";
+import Weather from "./Weather";
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+      <Weather defaultCity="Greece" />
+      <footer>
+        This project was coded by <a href="https://app.netlify.com/teams/mavooma/sites" target="_blank" rel="noreferrer">Vuyelwa Mavuma</a> and is open-sourced on {" "} <a href="https://github.com/Mavooma/weather-react-app" target="_blank" rel="noreferrer">GitHub</a>{" "}and <a href="https://weatherappreactmavooma.netlify.app/" target="_blank" rel="noopener noreferrer">hosted on Netlify</a>
+      </footer>
+      </div>
     </div>
   );
 }
 
-export default App;
